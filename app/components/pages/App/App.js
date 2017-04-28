fcec7709e6b90893bba8e0d0f5b1d72168d86651
Natalie Nash <natalienash@Natalies-MBP-2.home>
2017-04-27 20:35:11 -0400

    header

---
+++ app/components/pages/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from '../../views';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header
+          header={'Audrey\'s Great Big Book of Food'}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
